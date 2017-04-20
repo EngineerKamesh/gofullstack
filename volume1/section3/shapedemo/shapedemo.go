ec9861aab0cb92b26e010a7a3b76fac1c164f21b
Kamesh Balasubramanian <kamesh@wirecog.com>
2017-04-20 16:31:27 -0700

    Corrected typo.

--- volume1/section3/shapedemo/shapedemo.go
+++ volume1/section3/shapedemo/shapedemo.go
@@ -12,6 +12,6 @@ func main() {
 	t := simpleshape.NewTriangle(3, 6)
 
 	fmt.Println("Area of myRectangle: ", simpleshape.ShapeArea(r))
-	fmt.Println("Area of myCircle: ", simpleshape.ShapeArea(t))
+	fmt.Println("Area of myTriangle: ", simpleshape.ShapeArea(t))
 
 }
