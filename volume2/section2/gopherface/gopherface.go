12374398f1367af4c6f4ef02895dcc350fb7692a
Kamesh Balasubramanian <kamesh@wirecog.com>
2017-05-25 17:33:14 -0700

    Section 2 examples

---
+++ volume2/section2/gopherface/gopherface.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+
+	//ghandlers "github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	//"github.com/EngineerKamesh/gofullstack/volume2/section2/gopherface/endpoints"
+	"github.com/EngineerKamesh/gofullstack/volume2/section2/gopherface/handlers"
+	//"github.com/EngineerKamesh/gofullstack/volume2/section2/gopherface/middleware"
+)
+
+const (
+	WEBSERVERPORT = ":8080"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handlers.HomeHandler)
+	r.HandleFunc("/register", handlers.RegisterHandler).Methods("GET,POST")
+	r.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
+	r.HandleFunc("/logout", handlers.LogoutHandler).Methods("POST")
+	r.HandleFunc("/feed", handlers.FeedHandler).Methods("GET")
+	r.HandleFunc("/friends", handlers.FriendsHandler).Methods("GET")
+	r.HandleFunc("/find", handlers.FindHandler).Methods("GET,POST")
+	r.HandleFunc("/profile", handlers.MyProfileHandler).Methods("GET")
+	r.HandleFunc("/profile/{username}", handlers.ProfileHandler).Methods("GET")
+	r.HandleFunc("/triggerpanic", handlers.TriggerPanicHandler).Methods("GET")
+	r.HandleFunc("/foo", handlers.FooHandler).Methods("GET")
+
+	//r.HandleFunc("/restapi/socialmediapost/{username}", endpoints.FetchPostsEndpoint).Methods("GET")
+	//r.HandleFunc("/restapi/socialmediapost/{postid}", endpoints.CreatePostEndpoint).Methods("POST")
+	//r.HandleFunc("/restapi/socialmediapost/{postid}", endpoints.UpdatePostEndpoint).Methods("PUT")
+	//r.HandleFunc("/restapi/socialmediapost/{postid}", endpoints.DeletePostEndpoint).Methods("DELETE")
+
+	http.Handle("/", r)
+	//http.Handle("/", ghandlers.LoggingHandler(os.Stdout, r))
+	//http.Handle("/", middleware.PanicRecoveryHandler(ghandlers.LoggingHandler(os.Stdout, r)))
+	//http.Handle("/", middleware.ContextExampleHandler(middleware.PanicRecoveryHandler(ghandlers.LoggingHandler(os.Stdout, r))))
+
+	http.ListenAndServe(WEBSERVERPORT, nil)
+
+}
